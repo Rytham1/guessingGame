3ce64732f3d18f7614f1fdd60d1d8f3560f5c304
Rytham1 <Rythamdawar30@gmail.com>
2024-04-09 21:27:37 -0700

    main template

--- scripts.js
+++ scripts.js
@@ -1,94 +1,57 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
+const wordText = document.querySelector(".word"),
+  hintText = document.querySelector(".hint span"),
+  timeText = document.querySelector(".time b"),
+  inputField = document.querySelector("input"),
+  refreshBtn = document.querySelector(".refresh-word"),
+  checkBtn = document.querySelector(".check-word");
 
+let timer;
+let correctWord;
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
-];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
-
-
-// This function adds cards the page to display the data in the array
-function showCards() {
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
+const initTimer = maxTime => {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      maxTime--;
+      timeText.innerText = maxTime;
+    } else {
+      clearInterval(timer);
+      alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+      initGame();
     }
-}
+  }, 1000);
+};
 
-function editCardContent(card, newTitle, newImageURL) {
-    card.style.display = "block";
+const initGame = () => {
+  let randomObj = words[Math.floor(Math.random() * words.length)];
+  let wordArray = randomObj.word.split("");
+  for (let i = wordArray.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+  }
+  wordText.innerText = wordArray.join("");
+  hintText.innerText = randomObj.hint;
+  correctWord = randomObj.word.toLowerCase();
+  inputField.value = "";
+  inputField.setAttribute("maxlength", correctWord.length);
+  initTimer(60); // Set initial time here (e.g., 60 seconds)
+};
 
-    const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
+const checkWord = () => {
+  let userWord = inputField.value.toLowerCase();
+  if (!userWord) {
+    alert("Please enter the word to check!");
+    return;
+  }
+  if (userWord !== correctWord) {
+    alert(`Oops! ${userWord} is not the correct word`);
+  } else {
+    alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
+  }
+};
 
-    const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
 
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
-}
-
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
-}
-
-function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
-}
+// Call initGame to start the game initially
+initGame();
